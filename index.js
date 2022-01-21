abe34c70324b3895579c7619d233adb3cde2a1ec
Danish <danishzayan6@gmail.com>
2022-01-21 16:24:08 +0530

    error fixed

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+// ************************************************
+// *   Project Name: ASCII Converter              *
+// *   Contributers : Hafizur Rahman, Danish      *
+// *   Date: 21-Jan-2022                          *
+// ************************************************
 var key = document.querySelector("#key");
 var code = document.querySelector("#code");
 
 document.addEventListener("keypress", function (e) {
-  //   $("#key").slidToggle();
   key.textContent = e.key;
   code.textContent = e.charCode;
 });
