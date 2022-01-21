7a2668b63691a32732fe56fb6fafc67fe9118b7b
Hafizur Rahman <hafizurjay@gmail.com>
2022-01-21 17:52:32 +0530

    add readme.md and code refactoring

--- index.js
+++ index.js
@@ -3,10 +3,10 @@
 // *   Contributers : Hafizur Rahman, Danish      *
 // *   Date: 21-Jan-2022                          *
 // ************************************************
-var key = document.querySelector("#key");
-var code = document.querySelector("#code");
+var key = document.querySelector('#key');
+var code = document.querySelector('#code');
 
-document.addEventListener("keypress", function (e) {
-  key.textContent = e.key;
+document.addEventListener('keypress', function (e) {
+  e.charCode === 32 ? (key.textContent = 'space') : (key.textContent = e.key);
   code.textContent = e.charCode;
 });
